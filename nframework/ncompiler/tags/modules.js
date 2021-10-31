db724f96b9b91e288f575d8b2ad648f03c1fb814
Thanh Duy <55396370+silverbossTD@users.noreply.github.com>
2021-10-31 17:07:23 +0700

    Update modules.js

--- nframework/ncompiler/tags/modules.js
+++ nframework/ncompiler/tags/modules.js
@@ -7,17 +7,16 @@ tag.isAutoClose = true;
 tag.Compile = function(element, childsCode, code) {
     let inputs = tag.GetInputs(element, childsCode, code);
 
-    let code = `
-    `;
+    let result = ``;
 
     for (let i = 0; i < inputs.length; i++) {
-        code += `this.modules.push('${inputs[i]}');
+        result += `this.modules.push('${inputs[i]}');
         `;
     }
 
     return `
 
-        ${code}
+        ${result}
 
     `;
 }
